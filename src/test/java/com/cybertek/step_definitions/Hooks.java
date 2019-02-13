24730d4fc430df59e5f1becb4a7f4022f03b85aa
AlesiaVA <alesia.shuhunova@gmail.com>
2019-02-13 00:48:31 -0500

    added hooks class

---
+++ src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -0,0 +1,32 @@
+package com.cybertek.step_definitions;
+
+import com.cybertek.utilities.Driver;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+
+    @Before(order = 2)
+    public void setUp() {
+        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Driver.getDriver().manage().window().maximize();
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            // this line is for taking screenshot
+            final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            // this line is adding the screenshot to the report
+            scenario.embed(screenshot, "image/png");
+        }
+
+        Driver.closeDriver();
+    }
+
+}
